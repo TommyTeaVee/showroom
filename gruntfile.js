a63e3bb7aaeaf715de528aa2961042ffebf72b88
Kevin Bieri <k.bieri@4teamwork.ch>
2016-05-03 10:57:36 +0200

    Reindent lines

--- gruntfile.js
+++ gruntfile.js
@@ -7,34 +7,34 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON("package.json"),
 
     browserify: {
-     dist: {
-      options: {
-       paths: ["./src", "./test/spec"],
-       transform: [
-        ["babelify", { presets: "es2015" }]
-       ],
-       browserifyOptions: {
-         standalone: "<%= pkg.name %>"
-       }
-      },
-      files: {
-         "dist/<%= pkg.name %>.js": ["./src/<%= pkg.name %>.js"]
+      dist: {
+        options: {
+          paths: ["./src", "./test/spec"],
+          transform: [
+            ["babelify", { presets: "es2015" }]
+          ],
+          browserifyOptions: {
+            standalone: "<%= pkg.name %>"
+          }
+        },
+        files: {
+          "dist/<%= pkg.name %>.js": ["./src/<%= pkg.name %>.js"]
+        }
       }
-     }
     },
 
     watch: {
-     scripts: {
-      files: ["src/**/*.js"],
-      tasks: ["browserify"]
-     }
+      scripts: {
+        files: ["src/**/*.js"],
+        tasks: ["browserify"]
+      }
     }
   });
 
-   grunt.loadNpmTasks("grunt-browserify");
-   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-   grunt.registerTask("default", ["watch"]);
-   grunt.registerTask("build", ["browserify"]);
+  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", ["browserify"]);
 
 };
