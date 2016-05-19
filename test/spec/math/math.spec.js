0101007013191ad6e65bcbf33be8aa7b5b3b5e33
Kevin Bieri <k.bieri@4teamwork.ch>
2016-05-19 15:44:15 +0200

    Reindent lines.

--- test/spec/math/math.spec.js
+++ test/spec/math/math.spec.js
@@ -1,7 +1,7 @@
 import {calcCircumference} from "math/math";
 
 describe("Math", () => {
-    it("Should calculate the cirumference correctly based on radius", () => {
-        assert.equal(calcCircumference(200), 1256.637061436)
-    });
+  it("Should calculate the cirumference correctly based on radius", () => {
+    assert.equal(calcCircumference(200), 1256.637061436)
+  });
 });
